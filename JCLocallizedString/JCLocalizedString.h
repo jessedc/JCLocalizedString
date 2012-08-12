2d05ee6a40cd25c4aa242f793a6847c336a3b898
Jesse Collis <jesse@jcmultimedia.com.au>
2012-08-12 11:20:08 +1000

    Initial Commit (JCLocalizedString)

---
+++ JCLocallizedString/JCLocalizedString.h
@@ -0,0 +1,92 @@
+//
+//  JCLocalizedString.h
+//
+//  Created by Jesse Collis on 31/07/12.
+//  Copyright (c) 2012, Jesse Collis JC Multimedia Design. <jesse@jcmultimedia.com.au>
+//  All rights reserved.
+//
+//  * Redistribution and use in source and binary forms, with or without
+//   modification, are permitted provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+//
+
+#import <Foundation/Foundation.h>
+
+#define JCLocalizedString(key, description) \
+[[JCLocalizedManager sharedManager] localizedStringForKey:(key) value:nil table:nil]
+
+#define JCLocalizedStringFromTable(key, tbl, comment) \
+[[JCLocalizedManager sharedManager] localizedStringForKey:(key) value:nil table:(tbl)]
+
+#define JCLocalizedStringFromTableInBundle(key, tbl, bundle, comment) \
+[bundle JCLocalizedStringForKey:(key) value:nil table:(tbl)]
+
+#define JCLocalizedStringWithDefaultValue(key, tbl, bundle, val, comment) \
+[bundle JCLocalizedStringForKey:(key) value:(val) table:(tbl)]
+
+#define kJCLocalizedStringMainBundleInfoPlistKey @"JCLocalizationBundle"
+#define kJCLocalizedStringDefaultTableName @"Localizable"
+#define kJCLocalisedStringActivteLocalizationChangedNotification @"kJCLocalisedStringActivteLocalizationChangedNotification"
+
+// TODO: consider integrating auto updating locale
+
+@interface JCLocalizedManager : NSObject <NSCacheDelegate>{
+  @private
+  NSCache *_tableCache;
+}
+
+@property (nonatomic, readonly, strong) NSBundle *localizationBundle;
+@property (nonatomic, strong) NSString *activeLocalization;
+
++ (JCLocalizedManager *)sharedManager;
+
++ (NSBundle *)localizationBundle;
+
+- (id)initWithLocalizationBundle:(NSBundle *)localizationBundle;
+- (id)initWithLocalizationBundle:(NSBundle *)localizationBundle activeLocalization:(NSString *)localization;
+
+
+- (NSString *)localizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName;
+- (NSString *)localizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName bundle:(NSBundle *)bundle;
+
+- (NSDictionary *)loadStringsTable:(NSString *)tableName localization:(NSString *)localization bundle:(NSBundle *)bundle;
+
+@end
+
+@interface NSBundle (JCLocalizedManager)
+
+/*
+ key: The key for a string in the table identified by tableName.
+ value: The value to return if key is nil or if a localized string for key can’t be found in the table.
+ tableName: The receiver’s string table to search. If tableName is nil or is an empty string, the method attempts to use the table in Localizable.strings.
+ */
+
+// this can be called on any bundle, not just the localizationBundle, backs back onto JCLocalizedString
+- (NSString *)JCLocalizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName;
+
+
+/* Using the user default NSShowNonLocalizedStrings, you can alter the behavior of localizedStringForKey:value:table:
+ to log a message when the method can’t find a localized string. If you set this default to YES
+ (in the global domain or in the application’s domain), then when the method can’t find a localized string in the table,
+ it logs a message to the console and capitalizes key before returning it.
+ */
+
+
+@end
---
+++ JCLocallizedString/JCLocalizedString.m
@@ -0,0 +1,224 @@
+//
+//  JCLocalizedString.m
+//
+//  Created by Jesse Collis on 31/07/12.
+//  Copyright (c) 2012, Jesse Collis JC Multimedia Design. <jesse@jcmultimedia.com.au>
+//  All rights reserved.
+//
+//  * Redistribution and use in source and binary forms, with or without
+//   modification, are permitted provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+//
+
+#import "JCLocalizedString.h"
+
+#define kJCLocalizedStringStandardUserDefaultsStoredLocalizationKey @"kJCLocalizedStringStandardUserDefaultsStoredLocalizationKey"
+
+@implementation JCLocalizedManager
+@synthesize localizationBundle = _localizationBundle;
+@synthesize activeLocalization = _activeLocalization;
+
++ (JCLocalizedManager *)sharedManager
+{
+  static dispatch_once_t onceToken;
+  static JCLocalizedManager *localizedString = nil;
+
+  dispatch_once(&onceToken, ^{
+    localizedString = [[self alloc] initWithLocalizationBundle:[self localizationBundle]];
+  });
+
+  return localizedString;
+}
+
++ (NSBundle *)localizationBundle
+{
+  NSString *bundleName = [[NSBundle mainBundle] objectForInfoDictionaryKey:kJCLocalizedStringMainBundleInfoPlistKey];
+  NSString *bundlePath = [[NSBundle mainBundle] pathForResource:bundleName ofType:@"bundle"];
+  NSBundle *bundle = [NSBundle bundleWithPath:bundlePath];
+
+  return bundle ?: [NSBundle mainBundle];
+}
+
+- (id)init
+{
+  NSBundle *localizationBundle = [[self class] localizationBundle];
+  return [self initWithLocalizationBundle:localizationBundle];
+}
+
+- (id)initWithLocalizationBundle:(NSBundle *)localizationBundle
+{
+  NSString *localization = [self defaultLocalizationForBundle:localizationBundle];
+  return [self initWithLocalizationBundle:localizationBundle activeLocalization:localization];
+}
+
+- (id)initWithLocalizationBundle:(NSBundle *)localizationBundle activeLocalization:(NSString *)activeLocalization
+{
+  if (localizationBundle == nil)
+  {
+    @throw [NSException exceptionWithName:NSInvalidArgumentException reason:@"localization bundle must not be nil" userInfo:nil];
+  }
+  else if ([localizationBundle localizations] == nil)
+  {
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"localization bundle must contain some localizations" userInfo:nil];
+  }
+
+  if ((self = [super init]))
+  {
+    _localizationBundle = localizationBundle;
+#if !__has_feature(objc_arc)
+    [localizationBundle retain];
+#endif
+    self.activeLocalization = activeLocalization;
+    _tableCache = [[NSCache alloc] init];
+    _tableCache.delegate = self;
+    _tableCache.name = @"JCLocallizedString strings table cache";
+  }
+
+  return self;
+}
+
+- (void)dealloc
+{
+#if !__has_feature(objc_arc)
+  [_localizationBundle release];
+  [_activeLocalization release];
+  [_tableCache release];
+  [super dealloc];
+#endif
+}
+
+- (void)setActiveLocalization:(NSString *)activeLocalization
+{
+  if (activeLocalization == nil)
+  {
+    @throw [NSException exceptionWithName:NSInvalidArgumentException reason:@"active localization must not be nil" userInfo:nil];
+  }
+  else if (![[self.localizationBundle localizations] containsObject:activeLocalization])
+  {
+    NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys:activeLocalization,@"activeLocalization",self.localizationBundle,@"localizationBundle",nil];
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"active localization not found" userInfo:userInfo];
+  }
+
+  if (activeLocalization != _activeLocalization)
+  {
+#if __has_feature(objc_arc)
+    _activeLocalization = activeLocalization;
+#else
+    id old = _activeLocalization;
+    _activeLocalization = [activeLocalization retain];
+    [old release];
+#endif
+    
+    [[NSUserDefaults standardUserDefaults] setObject:_activeLocalization forKey:kJCLocalizedStringStandardUserDefaultsStoredLocalizationKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys:_activeLocalization,@"activeLocalization",nil];
+    [[NSNotificationCenter defaultCenter] postNotificationName:kJCLocalisedStringActivteLocalizationChangedNotification
+                                                        object:self
+                                                      userInfo:userInfo];
+  }
+}
+
+- (NSString *)localizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName
+{
+  return [self localizedStringForKey:key value:value table:tableName bundle:self.localizationBundle];
+}
+
+- (NSString *)localizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName bundle:(NSBundle *)bundle
+{
+  NSDictionary *stringsFileDictionary = [self loadStringsTable:tableName localization:self.activeLocalization bundle:bundle];
+    
+  NSString *stringToReturn = [stringsFileDictionary objectForKey:key];
+  
+  if (nil != stringToReturn) return stringToReturn;
+
+  if (nil != value) return value;
+
+  BOOL showNonLocalizedString = [[[NSUserDefaults standardUserDefaults] valueForKey:@"NSShowNonLocalizedStrings"] boolValue];
+  if (showNonLocalizedString)
+  {
+    NSLog(@"JCLocalizedManager: Unimplemented key '%@' for table '%@'", key, tableName);
+    return [key uppercaseString];
+  }
+
+  return key;
+}
+
+- (NSDictionary *)loadStringsTable:(NSString *)tableName localization:(NSString *)localization bundle:(NSBundle *)bundle
+{
+  tableName = tableName ?: kJCLocalizedStringDefaultTableName;
+  
+  NSString *stringsFilePath = [bundle pathForResource:tableName ofType:@"strings" inDirectory:nil forLocalization:localization];
+  NSAssert(stringsFilePath != nil, @"strings table not found in bundle");
+
+  NSDictionary *cachedDictionary = [_tableCache objectForKey:stringsFilePath];
+  if (cachedDictionary) return cachedDictionary;
+
+  NSString *stringsFile = [NSString stringWithContentsOfFile:stringsFilePath encoding:NSUTF8StringEncoding error:nil];
+  //NOTE: Apple recommends using NSPropertyList.h
+  NSDictionary *stringsDictionary = [stringsFile propertyListFromStringsFileFormat];
+
+  [_tableCache setObject:stringsDictionary forKey:stringsFilePath];
+
+  return stringsDictionary;
+}
+
+- (NSString *)defaultLocalizationForBundle:(NSBundle *)localizationBundle
+{
+  NSString *storedLocalization = [[NSUserDefaults standardUserDefaults] objectForKey:kJCLocalizedStringStandardUserDefaultsStoredLocalizationKey];
+
+  if (storedLocalization) return storedLocalization;
+
+  NSArray *preferredLanguages = [NSLocale preferredLanguages];
+  NSArray *bundleLocalizations = [localizationBundle localizations];
+
+  for (NSString *IETF_BCP_47_identifier in preferredLanguages)
+  {
+    if ([bundleLocalizations containsObject:IETF_BCP_47_identifier])
+    {
+      return IETF_BCP_47_identifier;
+    }
+  }
+
+  if ([bundleLocalizations count] > 0)
+  {
+    return [bundleLocalizations objectAtIndex:0];
+  }
+
+  return nil;
+}
+
+#pragma mark - NSCacheDelegate
+
+- (void)cache:(NSCache *)cache willEvictObject:(id)obj
+{
+  NSLog(@"CacheWillEvictObject: %@", obj);
+}
+
+@end
+
+@implementation NSBundle (JCLocalizedManager)
+
+- (NSString *)JCLocalizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName;
+{
+  return [[JCLocalizedManager sharedManager] localizedStringForKey:key value:value table:tableName bundle:self];
+}
+
+@end
